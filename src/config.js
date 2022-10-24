21f770ff5a94636d69e7cae72fff95710a5c2bf3
nandobfer <nandobfer@yahoo.com.br>
2022-10-24 15:58:55 -0300

    fix: conditional initial quantity

--- src/config.js
+++ src/config.js
@@ -1,6 +1,6 @@
 export const config = {
     stats: {
-        moondust: 10050,
+        moondust: 0,
         moondust_per_click: 1,
         moondust_per_click_bonus: 100,
         moondust_per_second: 0,
